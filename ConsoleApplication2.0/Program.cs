2009a3f630030ecc6a9e36b1914f08bac9d1e0d7
charmeleon-char <ramoncalixb@gmail.com>
2016-02-06 15:32:49 -0600

    first commit for App2.0

--- ConsoleApplication2.0/Program.cs
+++ ConsoleApplication2.0/Program.cs
@@ -126,9 +126,13 @@ namespace ConsoleApplication1
         }
         public void core()
         {
+            DateTime now = DateTime.Now;
+            var oneweekAffter = now.AddDays(7);
             DateTime day = DateTime.Today;
             int offset = day.DayOfWeek - DayOfWeek.Monday;
             DateTime lastmonday = Convert.ToDateTime(day.AddDays(-offset - 7));
+            
+            
             string monday = lastmonday.ToString("MM-dd-yyyy");
             string path = "";
             //  monday = ("12-14-2015");
@@ -234,7 +238,16 @@ namespace ConsoleApplication1
                 CreatePdf(items.name, items.userid, monday, false, path);
                 sendEmail(monday, items.name, items.name, items.email, "\\Employes\\");
             }
+            if (oneweekAffter.Month != now.Month)
+            {
 
+                Console.WriteLine("ultimo lunes ................................");
+            }
+            else
+            {
+
+                Console.WriteLine(" no es el ultimo lunes b................................");
+            }
         }
 
 
@@ -435,10 +448,6 @@ namespace ConsoleApplication1
 
 
             }
-
-
-
-
 
 
             foreach (var item in all)
@@ -699,6 +708,316 @@ namespace ConsoleApplication1
                     }
                 }
             }
+        }
+
+
+        public PdfPTable ArchiveSpin4(PdfPTable table, int deptID, string subdept, string dates, bool type)
+        {
+            List<test> all = new List<test>();
+            PdfPCell cell = new PdfPCell();
+            PdfPCell cell2 = new PdfPCell();
+            PdfPCell cellName = new PdfPCell();
+            PdfPCell cellMonday = new PdfPCell();
+            PdfPCell cellTuesday = new PdfPCell();
+            PdfPCell cellWednesday = new PdfPCell();
+            PdfPCell cellThursday = new PdfPCell();
+            PdfPCell cellfriday = new PdfPCell();
+            PdfPCell cellsaturday = new PdfPCell();
+            PdfPCell cellsunday = new PdfPCell();
+            Array[] list = { };
+            iTextSharp.text.Font georgia = FontFactory.GetFont("georgia", 14f);
+            georgia.Color = iTextSharp.text.BaseColor.BLUE;
+            iTextSharp.text.Font labels = FontFactory.GetFont("georgia", 14f);
+            labels.Color = iTextSharp.text.BaseColor.WHITE;
+            iTextSharp.text.Font Headers = FontFactory.GetFont("georgia", 18f);
+            Headers.Color = iTextSharp.text.BaseColor.BLACK;
+            List<test> concat = new List<test>();
+
+            PdfPTable table2 = new PdfPTable(24);
+            table.TotalWidth = 5000f;
+
+            float[] widths = new float[] { 1300f, 1300, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f, 1300f };
+            table.SetWidths(widths);
+
+
+            //  var init = Convert.ToInt32(deptID.ToString());
+            int count = -7;
+          
+            for (int i = 0; i < 3; i++)
+            {
+                
+                using (Anviz_Data_BaseEntities dc = new Anviz_Data_BaseEntities())
+                {
+                    //   all = (from e in dc).ToList();
+                    if (type == true)
+                    {
+                        all = (from e in dc.sp_matrix_weekdays(deptID, dates) select new test { Nombre = e.name, FechaEntradaMonday = e.Hora_de_LLegada_Lunes, FechaSalidaMonday = e.Hora_de_salida_Lunes, HorasMonday = e.Monday, FechaEntradaTuesday = e.Hora_de_LLegada_Martes, FechaSalidaTuesday = e.Hora_de_salida_Martes, HorasTuesday = e.Tuesday, FechaEntradaWendnsday = e.Hora_de_LLegada_Miercoles, FechaSalidaWendnsday = e.Hora_de_salida_Miercoles, HorasWendnsday = e.Wednsday, FechaEntradaThursday = e.Hora_de_LLegada_Jueves, FechaSalidaThursday = e.Hora_de_salida_Jueves, HorasThursday = e.Thursday, FechaEntradaFriday = e.Hora_de_LLegada_Viernes, FechaSalidaFriday = e.Hora_de_salida_Viernes, HorasFriday = e.Friday, FechaEntradaSaturday = e.Hora_de_LLegada_Sabado, FechaSalidaSaturday = e.Hora_de_salida_Sabado, HorasSaturday = e.Saturday, FechaEntradaSunday = e.Hora_de_LLegada_Domingo, FechaSalidaSunday = e.Hora_de_salida_Domingo, HorasSunday = e.Sunday }).ToList();
+
+                        //    WorkingHoursPerDay = (from i in dc.sp_GetWorkingHoursPerDay(1, Convert.ToDateTime("08-13-2015").AddDays(w)) select new WorkingPerDay { Name = i.Name, HourInput = i.horaEntrada, HourOutput = i.horasalida, workingHours = Convert.ToInt32(i.horaTrabajada) }).ToList();
+                        //     list[w] = WorkingHoursPerDay.ToList().ToArray();
+                    }
+                    else
+                    {
+                        all = (from e in dc.GetEmployes(deptID, dates) select new test { Nombre = e.name, FechaEntradaMonday = e.Hora_de_LLegada_Lunes, FechaSalidaMonday = e.Hora_de_salida_Lunes, HorasMonday = e.Monday, FechaEntradaTuesday = e.Hora_de_LLegada_Martes, FechaSalidaTuesday = e.Hora_de_salida_Martes, HorasTuesday = e.Tuesday, FechaEntradaWendnsday = e.Hora_de_LLegada_Miercoles, FechaSalidaWendnsday = e.Hora_de_salida_Miercoles, HorasWendnsday = e.Wednsday, FechaEntradaThursday = e.Hora_de_LLegada_Jueves, FechaSalidaThursday = e.Hora_de_salida_Jueves, HorasThursday = e.Thursday, FechaEntradaFriday = e.Hora_de_LLegada_Viernes, FechaSalidaFriday = e.Hora_de_salida_Viernes, HorasFriday = e.Friday, FechaEntradaSaturday = e.Hora_de_LLegada_Sabado, FechaSalidaSaturday = e.Hora_de_salida_Sabado, HorasSaturday = e.Saturday, FechaEntradaSunday = e.Hora_de_LLegada_Domingo, FechaSalidaSunday = e.Hora_de_salida_Domingo, HorasSunday = e.Sunday }).ToList();
+
+                    }
+            
+                }
+               
+                DateTime datee = Convert.ToDateTime(dates);
+                DateTime back = Convert.ToDateTime(datee.AddDays(-count));
+                count = count - 7;
+                dates = back.ToString();
+           
+            }
+
+            DateTime date = Convert.ToDateTime(dates);
+
+            //PdfPCell june = new PdfPCell(new Phrase("Cell 2",georgia));
+
+
+            cell2 = new PdfPCell(new Phrase(subdept, Headers));
+            cell2.HorizontalAlignment = Element.ALIGN_CENTER;
+            cell2.BackgroundColor = new iTextSharp.text.BaseColor(245, 92, 24);
+            cell2.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            cell2.Colspan = 24;
+            table.AddCell(cell2);
+
+
+
+
+            PdfPCell nombre = new PdfPCell(new Phrase("Name", labels));
+            nombre.BackgroundColor = new iTextSharp.text.BaseColor(89, 89, 89);
+            nombre.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            nombre.BorderWidthRight = 1;
+            nombre.Rowspan = 2;
+            nombre.Colspan = 2;
+            nombre.HorizontalAlignment = 1;
+            nombre.VerticalAlignment = 1;
+            table.AddCell(nombre);
+
+            PdfPCell june = new PdfPCell(new Phrase("Monday  " + "\n" + Convert.ToDateTime(date).ToShortDateString().ToString(), Headers));
+            june.BorderWidthRight = 1;
+            june.HorizontalAlignment = 1;
+            june.BackgroundColor = new iTextSharp.text.BaseColor(247, 150, 70);
+            june.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            june.Colspan = 3;
+            table.AddCell(june);
+            june = new PdfPCell(new Phrase("Tuesday  " + "\n" + Convert.ToDateTime(date).AddDays(1).ToShortDateString(), Headers));
+            june.BorderWidthRight = 1;
+            june.BackgroundColor = new iTextSharp.text.BaseColor(247, 150, 70);
+            june.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            june.HorizontalAlignment = 1;
+            june.Colspan = 3;
+            table.AddCell(june);
+            june = new PdfPCell(new Phrase("Wednesday  " + "\n" + Convert.ToDateTime(date).AddDays(2).ToShortDateString(), Headers));
+            june.BorderWidthRight = 1;
+            june.BackgroundColor = new iTextSharp.text.BaseColor(247, 150, 70);
+            june.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            june.HorizontalAlignment = 1;
+            june.Colspan = 3;
+            table.AddCell(june);
+            june = new PdfPCell(new Phrase("Thursday  " + "\n" + Convert.ToDateTime(date).AddDays(3).ToShortDateString(), Headers));
+            june.BorderWidthRight = 1;
+            june.BackgroundColor = new iTextSharp.text.BaseColor(247, 150, 70);
+            june.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            june.HorizontalAlignment = 1;
+            june.Colspan = 3;
+            table.AddCell(june);
+
+            june = new PdfPCell(new Phrase("Friday   " + "\n" + Convert.ToDateTime(date).AddDays(4).ToShortDateString(), Headers));
+            june.BorderWidthRight = 1;
+            june.BackgroundColor = new iTextSharp.text.BaseColor(247, 150, 70);
+            june.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            june.HorizontalAlignment = 1;
+            june.Colspan = 3;
+            table.AddCell(june);
+
+
+            june = new PdfPCell(new Phrase("Saturday  " + "\n" + Convert.ToDateTime(date).AddDays(5).ToShortDateString(), Headers));
+            june.BorderWidthRight = 1;
+            june.BackgroundColor = new iTextSharp.text.BaseColor(247, 150, 70);
+            june.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            june.HorizontalAlignment = 1;
+            june.Colspan = 3;
+            table.AddCell(june);
+            june = new PdfPCell(new Phrase("Sunday   " + "\n" + Convert.ToDateTime(date).AddDays(6).ToShortDateString(), Headers));
+            june.BorderWidthRight = 1;
+            june.BackgroundColor = new iTextSharp.text.BaseColor(247, 150, 70);
+            june.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            june.HorizontalAlignment = 1;
+            june.Colspan = 3;
+            table.AddCell(june);
+
+            june = new PdfPCell(new Phrase("Weekly Hours Worked", labels));
+            june.BorderWidthRight = 1;
+            june.Rowspan = 2;
+            june.HorizontalAlignment = 1;
+            june.BackgroundColor = new iTextSharp.text.BaseColor(89, 89, 89);
+            june.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+            table.AddCell(june);
+
+
+
+
+            PdfPCell In = new PdfPCell();
+            PdfPCell OUT = new PdfPCell();
+            PdfPCell second = new PdfPCell();
+
+            for (var y = 0; y <= 6; y++)
+            {
+                In = new PdfPCell(new Phrase("In", labels));
+
+                In.BackgroundColor = new iTextSharp.text.BaseColor(89, 89, 89);
+                In.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+                In.HorizontalAlignment = 1;
+                table.AddCell(In);
+
+                OUT = new PdfPCell(new Phrase("Out", labels));
+                OUT.BackgroundColor = new iTextSharp.text.BaseColor(89, 89, 89);
+                OUT.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+                OUT.HorizontalAlignment = 1;
+                table.AddCell(OUT);
+
+                second = new PdfPCell(new Phrase("Worked Hours", labels));
+                second.BackgroundColor = new iTextSharp.text.BaseColor(89, 89, 89);
+                second.BorderColor = new iTextSharp.text.BaseColor(0, 0, 0);
+                second.BorderWidthRight = 1;
+                second.HorizontalAlignment = 1;
+                table.AddCell(second);
+
+
+            }
+        
+            foreach (var item in all)
+            {
+
+                
+                cellName.Phrase = new Phrase(item.Nombre);
+                cellName.HorizontalAlignment = 1;
+                cellName.Colspan = 2;
+                cellName.BorderWidthRight = 1;
+                table.AddCell(cellName);
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaEntradaMonday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaSalidaMonday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                cellMonday = new PdfPCell(new Phrase((item.HorasMonday).ToString()));
+                cellMonday.BackgroundColor = new iTextSharp.text.BaseColor(242, 242, 242);
+                cellMonday.BorderWidthRight = 1;
+                cellMonday.HorizontalAlignment = 1;
+                table.AddCell(cellMonday);
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaEntradaTuesday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaSalidaTuesday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                cellTuesday = new PdfPCell(new Phrase((item.HorasTuesday).ToString()));
+                cellTuesday.BackgroundColor = new iTextSharp.text.BaseColor(242, 242, 242);
+                cellTuesday.BorderWidthRight = 1;
+                cellTuesday.HorizontalAlignment = 1;
+                table.AddCell(cellTuesday);
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaEntradaWendnsday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaSalidaWendnsday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                cellWednesday = new PdfPCell(new Phrase((item.HorasWendnsday).ToString()));
+                cellWednesday.BackgroundColor = new iTextSharp.text.BaseColor(242, 242, 242);
+                cellWednesday.BorderWidthRight = 1;
+                cellWednesday.HorizontalAlignment = 1;
+                table.AddCell(cellWednesday);
+
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaEntradaThursday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaSalidaThursday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                cellThursday = new PdfPCell(new Phrase((item.HorasThursday).ToString()));
+                cellThursday.BackgroundColor = new iTextSharp.text.BaseColor(242, 242, 242);
+                cellThursday.BorderWidthRight = 1;
+                cellThursday.HorizontalAlignment = 1;
+                table.AddCell(cellThursday);
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaEntradaFriday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaSalidaFriday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                cellfriday = new PdfPCell(new Phrase((item.HorasFriday).ToString()));
+                cellfriday.BackgroundColor = new iTextSharp.text.BaseColor(242, 242, 242);
+                cellfriday.BorderWidthRight = 1;
+                cellfriday.HorizontalAlignment = 1;
+                table.AddCell(cellfriday);
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaEntradaSaturday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaSalidaSaturday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                cellsaturday = new PdfPCell(new Phrase((item.HorasSaturday).ToString()));
+                cellsaturday.BackgroundColor = new iTextSharp.text.BaseColor(242, 242, 242);
+                cellsaturday.BorderWidthRight = 1;
+                cellsaturday.HorizontalAlignment = 1;
+                table.AddCell(cellsaturday);
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaEntradaSunday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+
+                cell.Phrase = new Phrase(Convert.ToDateTime(item.FechaSalidaSunday).ToShortTimeString().ToString());
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                cellsunday = new PdfPCell(new Phrase((item.HorasSunday).ToString()));
+                cellsunday.BackgroundColor = new iTextSharp.text.BaseColor(242, 242, 242);
+                cellsunday.BorderWidthRight = 1;
+                cellsunday.HorizontalAlignment = 1;
+                table.AddCell(cellsunday);
+
+                cell.Phrase = new Phrase(Convert.ToString(Convert.ToDecimal(item.HorasMonday) + Convert.ToDecimal(item.HorasTuesday) + Convert.ToDecimal(item.HorasWendnsday) + Convert.ToDecimal(item.HorasThursday) + Convert.ToDecimal(item.HorasFriday) + Convert.ToDecimal(item.HorasSaturday) + Convert.ToDecimal(item.HorasSunday)));
+                cell.HorizontalAlignment = 1;
+
+                table.AddCell(cell);
+
+                //table.Rows.Add(cell);
+                //                 
+
+            }
+
+
+
+            return table;
         }
 
     }
