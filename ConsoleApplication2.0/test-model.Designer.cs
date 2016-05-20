dd2522337b465cff38e5c1f1335cb4e0b59b8f10
Jose Eduardo Hernandez <joedhernandez95@unitec.edu>
2016-05-20 11:17:59 -0600

    agregando nueva funcion que pinta la casilla en los reportes dependiendo si marco de manera remote o no

--- ConsoleApplication2.0/test-model.Designer.cs
+++ ConsoleApplication2.0/test-model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\jhernandez\Desktop\OneDrive - Laureate Education\Assistance Report\ConsoleApplicationC-\ConsoleApplication2.0\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\jhernandez\Desktop\OneDrive - Laureate Education\Assistance Report\ConsoleApplicationC-\ConsoleApplication2.0\test-model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
